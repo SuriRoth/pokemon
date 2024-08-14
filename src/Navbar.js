529422c5cb9ab3bdac30b453a53beb3270d05a3f
SuriRoth <roth0725@gmail.com>
2024-08-14 14:01:56 -0400

    added navbar and adjustments

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar(){
+  return(
+  <nav className="navbar bg-body-tertiary h1">
+    <div className="container d-flex justify-content-center">
+      <a className="navbar-brand" href="/">
+        <img src="https://cdn-icons-png.flaticon.com/128/188/188987.png" alt="Logo" width="50" height="50" className="d-inline-block align-text-top"/>  Pokemon!
+        </a>
+    </div>
+  </nav>
+  )
+
+
+}
+
+export default Navbar;
